ddfac7f3f56f0a292b39515cfd61fc7b5437f83c
Amber <adm2037@gmail.com>
2018-09-09 21:13:22 -0500

    Added Contract and SQLiteOpenHelper

--- app/src/main/java/com/example/android/inventoryapp/data/BookContract.java
+++ app/src/main/java/com/example/android/inventoryapp/data/BookContract.java
@@ -1,4 +1,35 @@
 package com.example.android.inventoryapp.data;
 
-public class BookContract {
+import android.provider.BaseColumns;
+
+/**
+ * Schema and contract class {@link BookContract} for the book store database
+ */
+public final class BookContract {
+
+    // So as to not accidentally instantiate the class
+    private BookContract() {}
+
+    // Define constants for database
+    public static final class BookEntry implements BaseColumns {
+
+        // Table name
+        public final static String TABLE_NAME = "books";
+
+        // 8 columns: _id, title, author, price, type, quantity, supplier name, supplier phone
+        // String = data types for constants, not the values
+        public final static String _ID = BaseColumns._ID; // Type: INTEGER PRIMARY KEY AUTOINCREMENT
+        public final static String COLUMN_BOOK_TITLE = "title"; // Type: TEXT NOT NULL
+        public final static String COLUMN_BOOK_AUTHOR = "author"; // Type: TEXT NOT NULL
+        public final static String COLUMN_BOOK_PRICE = "price"; // Type: DOUBLE
+        public final static String COLUMN_BOOK_TYPE = "book_type"; // Type: INTEGER NOT NULL
+        public final static String COLUMN_BOOK_QUANTITY = "quantity"; // Type: INTEGER NOT NULL DEFAULT 0
+        public final static String COLUMN_BOOK_SUPPLIER_NAME = "supplier"; // Type: TEXT
+        public final static String COLUMN_BOOK_SUPPLIER_NUMBER = "number"; // Type: TEXT
+
+        // Book type constants:
+        public final static int BOOK_TYPE_UNKNOWN = 0;
+        public final static int BOOK_TYPE_HARDCOVER = 1;
+        public final static int BOOK_TYPE_PAPERBACK = 2;
+    }
 }
--- app/src/main/java/com/example/android/inventoryapp/data/BookDbHelper.java
+++ app/src/main/java/com/example/android/inventoryapp/data/BookDbHelper.java
@@ -1,4 +1,57 @@
 package com.example.android.inventoryapp.data;
 
-public class BookDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.android.inventoryapp.data.BookContract.BookEntry;
+
+
+/**
+ * Database helper for the Inventory App. Manages book database creation and version.
+ */
+public class BookDbHelper extends SQLiteOpenHelper {
+
+    public static final String LOG_TAG = BookDbHelper.class.getSimpleName();
+
+    // Name of database file
+    private static final String DATABASE_NAME = "bookstore.db";
+
+    // Database version-- increment if changing schema
+    private static final int DATABASE_VERSION = 1;
+
+    /**
+     * Constructs new instance of {@link BookDbHelper}.
+     * @param context of app [cursor = null because set to default]
+     */
+    public BookDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /**
+     * Called when database is first created
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Create String with SQL statement to create books table
+        String SQL_CREATE_BOOKS_TABLE = "CREATE TABLE " + BookEntry.TABLE_NAME + " ("
+                + BookEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + BookEntry.COLUMN_BOOK_TITLE + " TEXT NOT NULL, "
+                + BookEntry.COLUMN_BOOK_AUTHOR + " TEXT, "
+                + BookEntry.COLUMN_BOOK_PRICE + " DOUBLE, "
+                + BookEntry.COLUMN_BOOK_TYPE + " INTEGER NOT NULL, "
+                + BookEntry.COLUMN_BOOK_QUANTITY + " INTEGER NOT NULL DEFAULT 0, "
+                + BookEntry.COLUMN_BOOK_SUPPLIER_NAME + " TEXT, "
+                + BookEntry.COLUMN_BOOK_SUPPLIER_NUMBER + " TEXT);";
+
+        // Execute SQL statement
+        db.execSQL(SQL_CREATE_BOOKS_TABLE);
+        Log.v(LOG_TAG, SQL_CREATE_BOOKS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Do nothing here since version is still at 1
+    }
 }
